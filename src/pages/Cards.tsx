819700406317f5649ad4a72a0cf4f486da160e40
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-06-30 18:14:07 +0200

    done with sorting for all global cards

--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
@@ -11,31 +11,36 @@ import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 import QuestionAnswerTwoToneIcon from '@mui/icons-material/QuestionAnswerTwoTone';
 import HeadSidebar from '../components/SidebarResp';
+import { MenuItem, TextField } from '@mui/material';
 
 const DISPLAY_ALL_CARDS = gql`
-  query {
-    getAllCards {
-      id
+  query ($orderBy: Sort) {
+    getAllCards(orderBy: $orderBy) {
       question
       answer
+      id
     }
   }
 `;
 
 function DisplayAllCards() {
   const [isFlipped, setIsFlipped] = useState(false);
+  const [orderBy, setOrderBy] = useState();
   const [flippedCard, setFlippedCard] = useState(0);
 
-  const { loading, data } = useQuery(DISPLAY_ALL_CARDS);
+  const { refetch, loading, data } = useQuery(DISPLAY_ALL_CARDS, {
+    variables: {
+      orderBy: orderBy,
+    },
+  });
   const flipCard = (id: number, e: any) => {
     setIsFlipped(!isFlipped);
     setFlippedCard(id);
   };
 
-  const current = new Date();
-  const date = `${current.getDate()}/${
-    current.getMonth() + 1
-  }/${current.getFullYear()}`;
+  useEffect(() => {
+    refetch();
+  });
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -59,6 +64,25 @@ function DisplayAllCards() {
             <AssignmentRoundedIcon />
             Display All Cards
           </Typography>
+        </Grid>
+        <Grid container spacing={5} sx={{ margin: '50px 0 0 10px' }}>
+          <TextField
+            onChange={(e: any) => setOrderBy(e.target.value)}
+            value={orderBy}
+            label='sort'
+            select
+            variant='outlined'
+            sx={{ width: '200px' }}
+          >
+            {[
+              { value: 'asc', label: 'asc' },
+              { value: 'desc', label: 'desc' },
+            ].map((option) => (
+              <MenuItem key={option.label} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
         </Grid>
         {loading ? (
           <CircularProgress sx={{ margin: 30 }} />
